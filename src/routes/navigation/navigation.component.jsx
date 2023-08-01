9f3a43cc8e032658c3e312b06b9023de0671efca
Joseph <ralphlsulit@gmail.com>
2023-08-02 03:06:14 +0800

    style: fix card-details styling in tablet and desktop view

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,8 +11,8 @@ const Navigation = () => {
 
   return (
     <Fragment>
-      <section className='nav-container text-sm font-semibold'>
-        <div className='element-container flex items-center justify-between h-20'>
+      <section className='nav-container text-xs sm:text-sm font-semibold'>
+        <div className='element-container flex items-center justify-between h-20 lg:max-w-7xl lg:mx-auto'>
           <h1 className='cursor-pointer w-44' onClick={handleResetCountries}>Where in the world?</h1>
           <span className='flex items-center justify-between w-24'><Moon size={18}/>Dark Mode</span>
         </div>
